124a2dd05339ffb8e4652682a472c6efd6fbe612
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-04-24 11:30:04 +0300

    feat: Adding simulation base class

---
+++ application/controllers/Simulation.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Simulation extends CI_Controller {
+
+    public function simulation_list () {
+        
+    }
+    public function simulation () {
+
+    }
+    public function simulation_form() {
+
+    }
+}
